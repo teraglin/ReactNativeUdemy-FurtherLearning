f2c6879d5a0e36469972ea54b6a19facfb075989
Aidan Kirvan <67266954+teraglin@users.noreply.github.com>
2023-11-20 14:40:33 +1100

    Task: useWindowDimensions hook

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -6,14 +6,34 @@ import {
   Text,
   Animated,
   Easing,
-  Dimensions,
+  // Dimensions,
+  useWindowDimensions,
+  ScrollView,
 } from "react-native";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const { width, height } = useWindowDimensions;
   spinValue = new Animated.Value(0);
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+    overflow: "visible",
+  };
 
   // First set up animation
   Animated.loop(
@@ -32,33 +52,39 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
   });
 
   return (
-    <View style={styles.rootContainer}>
-      <Title>GAME OVER!</Title>
-      {/* <View style={styles.imageContainer}> */}
-      <Animated.View
-        style={[styles.animatedView, { transform: [{ rotate: spin }] }]}
-      >
-        <Image
-          style={styles.image}
-          source={require("../assets/images/tim-head.png")}
-        />
-      </Animated.View>
-      {/* </View> */}
-      <Text style={styles.summaryText}>
-        Your Phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
-        rounds to guess the number{" "}
-        <Text style={styles.highlight}>{userNumber}</Text>.
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start New Game </PrimaryButton>
-    </View>
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        {/* <View style={styles.imageContainer}> */}
+        <Animated.View
+          style={[styles.animatedView, { transform: [{ rotate: spin }] }]}
+        >
+          <Image
+            style={[styles.image, imageStyle]}
+            source={require("../assets/images/tim-head.png")}
+          />
+        </Animated.View>
+        {/* </View> */}
+        <Text style={styles.summaryText}>
+          Your Phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
+          rounds to guess the number{" "}
+          <Text style={styles.highlight}>{userNumber}</Text>.
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start New Game </PrimaryButton>
+      </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get("window").width;
+// const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    overflow: "visible",
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -66,9 +92,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
-    borderRadius: deviceWidth < 380 ? 75 : 150,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
+    // borderRadius: deviceWidth < 380 ? 75 : 150,
     // borderWidth: 3,
     // borderColor: Colors.primary800,
     // backgroundColor: Colors.accent500,
@@ -80,8 +106,8 @@ const styles = StyleSheet.create({
   image: {
     // width: "100%",
     // height: "100%",
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
     marginBottom: -700,
     shadowRadius: 24,
     shadowColor: "black",
